9f0e38413f51348c1452a595a33610f84497cd4e
khan Tanveer <kt224945@gmail.com>
2021-04-02 15:21:07 +0530

    commit

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-const Product = ({ title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispatch the items into data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
-    <div className="product">
+    <div key={id} className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
@@ -20,7 +36,7 @@ const Product = ({ title, image, price, rating }) => {
       </div>
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 };
