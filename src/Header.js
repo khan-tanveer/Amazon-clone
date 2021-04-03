062a8da9e24facdc988b90d11621bf5f940e9f39
khan Tanveer <kt224945@gmail.com>
2021-04-03 16:33:38 +0530

    commit

--- src/Header.js
+++ src/Header.js
@@ -32,14 +32,18 @@ const Header = () => {
       {/* header nav */}
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne"> hello user</span>
-          <span className="header__optionLineTwo"> Sign In</span>
-        </div>
+        <Link to="/login">
+          <div className="header__option">
+            <span className="header__optionLineOne"> hello user</span>
+            <span className="header__optionLineTwo"> Sign In</span>
+          </div>
+        </Link>
+
         <div className="header__option">
           <span className="header__optionLineOne"> Returns </span>
           <span className="header__optionLineTwo"> orders</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne"> your</span>
           <span className="header__optionLineTwo"> prime</span>
---
+++ src/Login.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    // some fancy firebse login shitttttttttttttt
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    //here do some fancy firebase register shittttttttt
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+          />
+
+          <h5>Password</h5>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+
+          <button onClick={signIn} className="login__signInButton">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to Amazon's Fake clone Condition of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create Your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
