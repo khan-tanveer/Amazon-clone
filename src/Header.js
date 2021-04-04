545125fe7fc9b6f4b24ec9c0392217c39cccf7e3
khan Tanveer <kt224945@gmail.com>
2021-04-04 16:05:36 +0530

    commit

--- src/Header.js
+++ src/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
       <div className="header__nav">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
-            <span className="header__optionLineOne"> hello user</span>
+            <span className="header__optionLineOne"> hello {user?.email}</span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
